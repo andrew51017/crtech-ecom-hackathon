554973e6dc00d3a840b3dab6f55a81bde5d3435c
Andrew Fletcher <afletcher04@qub.ac.uk>
2015-11-21 13:43:08 +0000

    Remote methods.

--- server/models/application-user.js
+++ server/models/application-user.js
@@ -1,3 +1,17 @@
 module.exports = function(ApplicationUser) {
 
+    ApplicationUser.getWord =  function(cb)
+    {
+        cb(null, "Hello!");
+    };
+
+    ApplicationUser.remoteMethod('getWord',
+      {
+        http: {path: '/word', verb: 'get'},
+        returns: {arg: 'word', type: 'string'}
+      }
+    );
+
+
+
 };
--- server/models/models.js
+++
@@ -1,7 +0,0 @@
-var srv = require("../server");
-var ds = server.dataSources.db;
-
-var applicationUser = require('ApplicationUser');
-ds.createModel(applicationUser.name, applicationUser.properties, applicationUser.options);
-
-ds.automigrate();
