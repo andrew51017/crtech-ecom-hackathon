00de31125f582c179afa421e294e2f2151390201
neil_armstrong_1991@hotmail.com <neil@Neils-MacBook-Pro.local>
2015-11-21 14:03:11 +0000

    Added variable to example rest endpoints for naming rest methods.

--- server/models/application-user.js
+++ server/models/application-user.js
@@ -1,17 +1,12 @@
 module.exports = function(ApplicationUser) {
+  var getWord = "getWord";
+  ApplicationUser[getWord] =  function(callback) {
+    callback(null, "Hello!");
+  };
 
-    ApplicationUser.getWord =  function(cb)
-    {
-        cb(null, "Hello!");
-    };
-
-    ApplicationUser.remoteMethod('getWord',
-      {
-        http: {path: '/word', verb: 'get'},
-        returns: {arg: 'word', type: 'string'}
-      }
-    );
-
-
-
+  ApplicationUser.remoteMethod(getWord, {
+      http: {path: '/word', verb: 'get'},
+      returns: {arg: 'word', type: 'string'}
+    }
+  );
 };
